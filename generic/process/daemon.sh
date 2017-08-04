9eb9e59f67c9a1bef15a38166972405c7a9574aa
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2017-08-03 20:15:25 -0400

    Experimental daemonStatus function.

--- generic/process/daemon.sh
+++ generic/process/daemon.sh
@@ -1,18 +1,25 @@
+#True if daemon is running.
+_daemonStatus() {
+	ps -e | grep "$daemonPID" >/dev/null 2>&1 && return 0
+	return 1
+}
+
+
 _waitForTermination() {
-	ps -e | grep "$daemonPID" >/dev/null 2>&1 && sleep 0.1
-	ps -e | grep "$daemonPID" >/dev/null 2>&1 && sleep 0.3
-	ps -e | grep "$daemonPID" >/dev/null 2>&1 && sleep 1
-	ps -e | grep "$daemonPID" >/dev/null 2>&1 && sleep 2
+	_daemonStatus && sleep 0.1
+	_daemonStatus && sleep 0.3
+	_daemonStatus && sleep 1
+	_daemonStatus && sleep 2
 }
 alias _waitForDaemon=_waitForTermination
 
 #Kills background process using PID file.
 _killDaemon() {
-	ps -e | grep "$daemonPID" >/dev/null 2>&1 && kill -TERM "$daemonPID" >/dev/null 2>&1
+	_daemonStatus && kill -TERM "$daemonPID" >/dev/null 2>&1
 	
 	_waitForTermination
 	
-	ps -e | grep "$daemonPID" >/dev/null 2>&1 && kill -KILL "$daemonPID" >/dev/null 2>&1
+	_daemonStatus && kill -KILL "$daemonPID" >/dev/null 2>&1
 	
 	_waitForTermination
 	
@@ -21,6 +28,6 @@ _killDaemon() {
 
 #Executes self in background (ie. as daemon).
 _execDaemon() {
-	"$scriptAbsoluteLocation" &
+	"$scriptAbsoluteLocation" > "$logTmp"/daemon 2>&1 &
 	echo "$!" > "$pidFile"
 }
