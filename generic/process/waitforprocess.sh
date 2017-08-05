54b944d8f337a27a80b737d5e6b8d87212f83922
Matthew mirage335 Hines <spamfreemirage335@gmail.com>
2017-08-05 13:11:59 -0400

    Comment regarding use of wait command.

--- generic/process/waitforprocess.sh
+++ generic/process/waitforprocess.sh
@@ -1,4 +1,4 @@
-#Waits for the process PID specified by first parameter to end. Useful in conjunction with $! to provide process control and/or PID files.
+#Waits for the process PID specified by first parameter to end. Useful in conjunction with $! to provide process control and/or PID files. Unlike wait command, does not require PID to be a child of the current shell.
 _pauseForProcess() {
 	while ps --no-headers -p $1 &> /dev/null
 	do
