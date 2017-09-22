fc60337381afe47289e1bfeef6c8a07ff0288b2e
mirage335 <spamfreemirage335@gmail.com>
2017-09-22 14:56:49 -0400

    ChRoot process management functions.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -416,6 +416,53 @@ _localDir() {
 	
 }
 
+#Lists all chrooted processes. First parameter is chroot directory. Script might need to run as root.
+#Techniques originally released by other authors at http://forums.grsecurity.net/viewtopic.php?f=3&t=1632 .
+#"$1" == ChRoot Dir
+_listprocChRoot() {
+	CHROOT=$(_getAbsoluteLocation "$1")
+	PROCS=""
+	for p in `ps -o pid -A`; do
+		if [ "`readlink /proc/$p/root`" = "$CHROOT" ]; then
+			PROCS="$PROCS $p"
+		fi
+	done
+	echo "$PROCS"
+}
+
+#End user and diagnostic function, shuts down all processes in a chroot.
+_stopChRoot() {
+	
+	ChRootDir=$(_getAbsoluteLocation "$1")
+	
+	echo "TERMinating all chrooted processes."
+	sleep 5
+	kill -TERM $(_listprocChRoot "$ChRootDir") >/dev/null 2>&1
+	sleep 15
+	
+	echo "KILLing all chrooted processes."
+	kill -KILL $(_listprocChRoot "$ChRootDir") >/dev/null 2>&1
+	sleep 1
+	
+	echo "Remaining chrooted processes."
+	_listprocChRoot "$ChRootDir"
+	
+	echo '-----'
+	
+}
+
+
+
+
+
+ 
+
+ 
+
+ 
+
+ 
+
 #Determines if user is root. If yes, then continue. If not, exits after printing error message.
 _mustBeRoot() {
 if [[ $(id -u) != 0 ]]; then 
