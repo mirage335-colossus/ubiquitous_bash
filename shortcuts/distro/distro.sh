9892090764c8aea692611868a997df47acaa6a10
mirage335 <spamfreemirage335@gmail.com>
2017-09-23 19:38:54 -0400

    Raspbian virtualization support in progress.

--- shortcuts/distro/distro.sh
+++ shortcuts/distro/distro.sh
@@ -1,4 +1,5 @@
 _testDistro() {
+	_checkDep sha256sum
 	_checkDep sha512sum
 	_checkDep axel
 }
---
+++ shortcuts/distro/raspbian/createRaspbian.sh
@@ -0,0 +1,43 @@
+_fetch_raspbian_sequence() {
+	_start
+	
+	export functionEntryPWD="$PWD"
+	
+	export storageLocation="$scriptAbsoluteFolder"/_lib/os/
+	[[ "$1" != "" ]] && export storageLocation=$(_getAbsoluteLocation "$1")
+	
+	cd "$safeTmp"
+	
+	[[ -e "$storageLocation"/2017-09-07-raspbian-stretch.zip ]] && cp "$storageLocation"/2017-09-07-raspbian-stretch.zip ./2017-09-07-raspbian-stretch.zip > /dev/null 2>&1
+	[[ -e ./2017-09-07-raspbian-stretch.zip ]] || _fetch 'https://downloads.raspberrypi.org/raspbian/images/raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip'
+	
+	wget https://downloads.raspberrypi.org/raspbian/images/raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip.sha256
+	
+	if ! cat raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip.sha256 | grep 2017-09-07-raspbian-stretch.zip | sha256sum -c - > /dev/null 2>&1
+	then
+		echo 'invalid'
+		_stop 1
+	fi
+	
+	#Raspbian signature is difficult to authenticate. Including hash here allows some trust to be established from a Git/SSH server, as well HTTPS generally.
+	if [[ "$(cat raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip.sha256 | cut -f1 -d\  )" != "a64d742bc525b548f0435581fac5876b50a4e9ba1d1cd6433358b4ab6c7a770b" ]]
+	then
+		echo 'invalid'
+		_stop 1
+	fi
+	
+	mkdir -p "$storageLocation"
+	
+	cd "$functionEntryPWD"
+	mv "$safeTmp"/raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip "$storageLocation"
+	
+	
+	
+	_stop
+}
+
+_fetch_raspbian() {
+	
+	"$scriptAbsoluteLocation" _fetch_raspbian_sequence "$@"
+	
+}
