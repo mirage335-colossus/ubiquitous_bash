18ccbbf01e2a82828a3faf0d24c26984a82e3f16
mirage335 <spamfreemirage335@gmail.com>
2017-09-23 21:05:02 -0400

    Raspbian fetch in progress.

--- shortcuts/distro/raspbian/createRaspbian.sh
+++ shortcuts/distro/raspbian/createRaspbian.sh
@@ -13,14 +13,14 @@ _fetch_raspbian_sequence() {
 	
 	wget https://downloads.raspberrypi.org/raspbian/images/raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip.sha256
 	
-	if ! cat raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip.sha256 | grep 2017-09-07-raspbian-stretch.zip | sha256sum -c - > /dev/null 2>&1
+	if ! cat 2017-09-07-raspbian-stretch.zip.sha256 | grep 2017-09-07-raspbian-stretch.zip | sha256sum -c - > /dev/null 2>&1
 	then
 		echo 'invalid'
 		_stop 1
 	fi
 	
 	#Raspbian signature is difficult to authenticate. Including hash here allows some trust to be established from a Git/SSH server, as well HTTPS generally.
-	if [[ "$(cat raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip.sha256 | cut -f1 -d\  )" != "a64d742bc525b548f0435581fac5876b50a4e9ba1d1cd6433358b4ab6c7a770b" ]]
+	if [[ "$(cat 2017-09-07-raspbian-stretch.zip.sha256 | cut -f1 -d\  )" != "a64d742bc525b548f0435581fac5876b50a4e9ba1d1cd6433358b4ab6c7a770b" ]]
 	then
 		echo 'invalid'
 		_stop 1
@@ -29,7 +29,7 @@ _fetch_raspbian_sequence() {
 	mkdir -p "$storageLocation"
 	
 	cd "$functionEntryPWD"
-	mv "$safeTmp"/raspbian-2017-09-08/2017-09-07-raspbian-stretch.zip "$storageLocation"
+	mv "$safeTmp"/2017-09-07-raspbian-stretch.zip "$storageLocation"
 	
 	
 	
