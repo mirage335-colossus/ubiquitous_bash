5e3992d34d839bbb8d3bfacf77906057573c3b0a
mirage335 <spamfreemirage335@gmail.com>
2017-11-24 16:40:37 -0500

    Conversion of raw disk images to tar format theoretically usable by Docker.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -4100,16 +4100,53 @@ _create_docker_container_instanced() {
 	return 0
 }
 
+_docker_img_to_tar_sequence() {
+	_mustGetSudo
+	
+	_start
+	
+	export functionEntryPWD="$PWD"
+	
+	_openImage
+	
+	
+	
+	cd "$globalVirtFS"
+	
+	sudo -n tar --selinux --acls --xattrs -cpf "$scriptLocal"/"dockerImageFS".tar ./
+	
+	cd "$functionEntryPWD"
+	
+	
+	
+	cd "$functionEntryPWD"
+	_closeImage
+	
+	_stop
+}
+
+
 _docker_img_to_tar() {
 	_messageProcess "Searching conflicts"
 	[[ -e "$scriptLocal"/"dockerContainerFS".tar ]] && _messageFAIL && return 1
 	[[ -e "$scriptLocal"/"dockerImageFS".tar ]] && _messageFAIL && return 1
 	[[ -e "$scriptLocal"/"dockerImageAll".tar ]] && _messageFAIL && return 1
+	! [[ -e "$scriptLocal"/vm.img ]] && _messageFAIL && return 1
+	
 	_messagePASS
 	
 	
+	"$scriptAbsoluteLocation" _docker_img_to_tar_sequence
 	
-	false
+	_messageProcess "Validating readiness"
+	_readLocked "$lock_open" && _messageFAIL && return 1
+	_readLocked "$lock_opening" && _messageFAIL && return 1
+	_readLocked "$lock_closing" && _messageFAIL && return 1
+	_readLocked "$lock_emergency" && _messageFAIL && return 1
+	_messagePASS
+	
+	
+	return 0
 }
 
 _docker_tar_to_img_sequence() {
@@ -4139,10 +4176,8 @@ _docker_tar_to_img_sequence() {
 	
 	
 	
-	
-	_closeImage
-	
 	cd "$functionEntryPWD"
+	_closeImage
 	
 	_stop
 }
@@ -4152,6 +4187,7 @@ _docker_tar_to_img() {
 	_messageProcess "Searching conflicts"
 	[[ -e "$scriptLocal"/vm.img ]] && _messageFAIL && return 1
 	[[ -e "$scriptLocal"/vm*.img ]] && _messageFAIL && return 1
+	! [[ -e "$scriptLocal"/"dockerImageFS".tar ]] && _messageFAIL && return 1
 	_messagePASS
 	
 	_messageProcess "Creating raw"
