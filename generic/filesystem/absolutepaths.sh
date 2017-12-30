1c0651211307b736c309de67b247a77523cfb95f
mirage335 <spamfreemirage335@gmail.com>
2017-12-30 17:19:29 -0500

    Test for problem reported with Mac.

--- generic/filesystem/absolutepaths.sh
+++ generic/filesystem/absolutepaths.sh
@@ -1,7 +1,22 @@
+#Critical prerequsites.
+_getAbsolute_criticalDep() {
+	! type realpath > /dev/null 2>&1 && return 1
+	! type readlink > /dev/null 2>&1 && return 1
+	! type dirname > /dev/null 2>&1 && return 1
+	
+	#Known issue on Mac. See https://github.com/mirage335/ubiquitous_bash/issues/1 .
+	! realpath -L . > /dev/null 2>&1 && return 1
+	
+	return 0
+}
+! _getAbsolute_criticalDep && exit 1
+
 #Retrieves absolute path of current script, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #However, will dereference symlinks IF the script location itself is a symlink. This is to allow symlinking to scripts to function normally.
 #Suitable for allowing scripts to find other scripts they depend on. May look like an ugly hack, but it has proven reliable over the years.
 _getScriptAbsoluteLocation() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$0" == "-"* ]]
 	then
 		return 1
@@ -28,6 +43,8 @@ alias getScriptAbsoluteLocation=_getScriptAbsoluteLocation
 #Retrieves absolute path of current script, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #Suitable for allowing scripts to find other scripts they depend on.
 _getScriptAbsoluteFolder() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$0" == "-"* ]]
 	then
 		return 1
@@ -40,6 +57,8 @@ alias getScriptAbsoluteFolder=_getScriptAbsoluteFolder
 #Retrieves absolute path of parameter, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #Suitable for finding absolute paths, when it is desirable not to interfere with symlink specified folder structure.
 _getAbsoluteLocation() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$1" == "-"* ]]
 	then
 		return 1
@@ -66,6 +85,8 @@ alias getAbsoluteLocation=_getAbsoluteLocation
 #Retrieves absolute path of parameter, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #Suitable for finding absolute paths, when it is desirable not to interfere with symlink specified folder structure.
 _getAbsoluteFolder() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$1" == "-"* ]]
 	then
 		return 1
--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -8,10 +8,25 @@ _showCommand() {
 	"$@"
 }
 
+#Critical prerequsites.
+_getAbsolute_criticalDep() {
+	! type realpath > /dev/null 2>&1 && return 1
+	! type readlink > /dev/null 2>&1 && return 1
+	! type dirname > /dev/null 2>&1 && return 1
+	
+	#Known issue on Mac. See https://github.com/mirage335/ubiquitous_bash/issues/1 .
+	! realpath -L . > /dev/null 2>&1 && return 1
+	
+	return 0
+}
+! _getAbsolute_criticalDep && exit 1
+
 #Retrieves absolute path of current script, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #However, will dereference symlinks IF the script location itself is a symlink. This is to allow symlinking to scripts to function normally.
 #Suitable for allowing scripts to find other scripts they depend on. May look like an ugly hack, but it has proven reliable over the years.
 _getScriptAbsoluteLocation() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$0" == "-"* ]]
 	then
 		return 1
@@ -38,6 +53,8 @@ alias getScriptAbsoluteLocation=_getScriptAbsoluteLocation
 #Retrieves absolute path of current script, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #Suitable for allowing scripts to find other scripts they depend on.
 _getScriptAbsoluteFolder() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$0" == "-"* ]]
 	then
 		return 1
@@ -50,6 +67,8 @@ alias getScriptAbsoluteFolder=_getScriptAbsoluteFolder
 #Retrieves absolute path of parameter, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #Suitable for finding absolute paths, when it is desirable not to interfere with symlink specified folder structure.
 _getAbsoluteLocation() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$1" == "-"* ]]
 	then
 		return 1
@@ -76,6 +95,8 @@ alias getAbsoluteLocation=_getAbsoluteLocation
 #Retrieves absolute path of parameter, while maintaining symlinks, even when "./" would translate with "readlink -f" into something disregarding symlinked components in $PWD.
 #Suitable for finding absolute paths, when it is desirable not to interfere with symlink specified folder structure.
 _getAbsoluteFolder() {
+	! _getAbsolute_criticalDep && return 1
+	
 	if [[ "$1" == "-"* ]]
 	then
 		return 1
