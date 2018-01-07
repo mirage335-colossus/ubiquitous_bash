286367c9a06de94b965824145c88063385263350
mirage335 <spamfreemirage335@gmail.com>
2018-01-06 19:09:10 -0500

    Mine status checker integrated.

--- shortcuts/blockchain/ethereum/ethereum.sh
+++ shortcuts/blockchain/ethereum/ethereum.sh
@@ -14,8 +14,12 @@ _ethereum_mine() {
 	_ethereum_home "$scriptBin"/ethminer -G --farm-recheck 200 -S eu1.ethermine.org:4444 -FS us1.ethermine.org:4444 -O "$ethaddr"."$rigname"
 }
 
+_ethereum_mine_status() {
+	https://ethermine.org/miners/"$ethaddr"
+}
+
 # TODO Dynamically chosen port.
-_parity_browser() {
+_parity_ui() {
 	#xdg-open 'http://127.0.0.1:'"$parity_ui_port"'/#/'
 	
 	#egrep -o 'https?://[^ ]+'
