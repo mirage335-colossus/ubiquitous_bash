d13d2164d50224c798ffbe14187657f441775b0e
mirage335 <spamfreemirage335@gmail.com>
2018-01-27 03:06:48 -0500

    Compile.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -6904,6 +6904,35 @@ _parity_attach() {
 	_ethereum_home _geth attach ~/.local/share/io.parity.ethereum/jsonrpc.ipc "$@"
 }
 
+#####Network Specific Variables
+#Statically embedded into monolithic cautossh script by compile script .
+
+export netName=default
+export gatewayName="$netName"-gw
+export LOCALSSHPORT=22
+
+#Set to the desktop user most commonly logged in.
+#[[ "$SSHUSER" == "" ]] && export SSHUSER=
+#[[ "$X11USER" == "" ]] && export X11USER=
+
+
+#Example ONLY. Modify port asignments.
+if [[ "$reversePort" == "" ]]
+then
+	export reversePort=20009
+	case $(hostname -s) in
+		alpha)
+			export reversePort=20000
+			;;
+		beta)
+			export reversePort=20001
+			export EMBEDDED=true
+			;;
+	esac
+fi
+
+export keepKeys=true
+
 #####Basic Variable Management
 
 #####Global variables.
