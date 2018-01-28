8498e5f5e54b182e7139154e8bef102ebe5bdfd5
mirage335 <spamfreemirage335@gmail.com>
2018-01-27 23:39:13 -0500

    Merging cautossh functionality.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -864,6 +864,8 @@ _testProxyRouter_sequence() {
 
 _testProxyRouter() {
 	_getDep nc
+	_getDep nmap
+	_getDep ssh
 	
 	if "$scriptAbsoluteLocation" _testProxyRouter_sequence "$@"
 	then
@@ -884,6 +886,53 @@ _proxy() {
 	nc "$proxyTargetHost" "$proxyTargetPort"
 }
 
+#Enters remote server at hostname, by SSH, sets up a tunnel, checks tunnel for another SSH server.
+#"$1" == hostname
+#"$2" == port
+_testRemotePort() {
+	local localPort
+	localPort=$(_findPort)
+	
+	_timeout 18 ssh -F "$sshDir"/config "$1" -L "$localPort":localhost:"$2" -N > /dev/null 2>&1 &
+	sleep 2
+	nmap -Pn localhost -p "$localPort" -sV | grep 'ssh' > /dev/null 2>&1 && return 0
+	sleep 2
+	nmap -Pn localhost -p "$localPort" -sV | grep 'ssh' > /dev/null 2>&1 && return 0
+	sleep 2
+	nmap -Pn localhost -p "$localPort" -sV | grep 'ssh' > /dev/null 2>&1 && return 0
+	sleep 2
+	nmap -Pn localhost -p "$localPort" -sV | grep 'ssh' > /dev/null 2>&1 && return 0
+	sleep 6
+	nmap -Pn localhost -p "$localPort" -sV | grep 'ssh' > /dev/null 2>&1 && return 0
+}
+
+#Checks hostname for open port.
+#"$1" == hostname
+#"$2" == port
+_testPort() {
+	nmap -Pn "$1" -p "$2" | grep open > /dev/null 2>&1
+}
+
+_ssh() {
+	ssh -F "$scriptLocal"/ssh/config "$@"
+}
+
+#Builtin version of ssh-copy-id.
+_ssh_copy_id() {
+	_start
+	
+	"$scriptAbsoluteLocation" _ssh "$@" 'mkdir -p "$HOME"/.ssh'
+	cat "$scriptAbsoluteFolder"/id_rsa.pub | "$scriptAbsoluteLocation" _ssh "$@" 'cat - >> "$HOME"/.ssh/authorized_keys'
+	
+	_stop
+}
+alias _ssh-copy-id=_ssh_copy_id
+
+#Overload with "ops".
+_setup_ssh_extra() {
+	true
+}
+
 _setup_ssh() {
 	! [[ -e ~/.ssh ]] && mkdir -p ~/.ssh && chmod 700 ~/.ssh
 	! [[ -e ~/.ssh/"$ubiquitiousBashID" ]] && mkdir -p ~/.ssh/"$ubiquitiousBashID" && chmod 700 ~/.ssh/"$ubiquitiousBashID"
@@ -893,13 +942,23 @@ _setup_ssh() {
 	
 	! grep "$netName" ~/.ssh/"$ubiquitiousBashID"/config > /dev/null 2>&1 && echo "Include "'"'"~/.ssh/""$ubiquitiousBashID""/""$netName""/config"'"' >> ~/.ssh/"$ubiquitiousBashID"/config
 	
+	if ! [[ -e "$scriptLocal"/ssh/id_rsa ]] && ! [[ -e ~/.ssh/"$ubiquitiousBashID"/"$netName"/id_rsa ]]
+	then
+		ssh-keygen -b 4096 -t rsa -N "" -f "$scriptLocal"/id_rsa
+	fi
+	
+	chmod 600 "$scriptLocal"/ssh/id_rsa
+	chmod 600 "$scriptLocal"/ssh/id_rsa.pub
+	
 	_cpDiff "$scriptLocal"/ssh/config ~/.ssh/"$ubiquitiousBashID"/"$netName"/config
-	_cpDiff "$scriptLocal"/ssh/id_rsa ~/.ssh/"$ubiquitiousBashID"/"$netName"/id_rsa
-	_cpDiff "$scriptLocal"/ssh/id_rsa.pub ~/.ssh/"$ubiquitiousBashID"/"$netName"/id_rsa.pub
+	cp -n "$scriptLocal"/ssh/id_rsa ~/.ssh/"$ubiquitiousBashID"/"$netName"/id_rsa
+	cp -n "$scriptLocal"/ssh/id_rsa.pub ~/.ssh/"$ubiquitiousBashID"/"$netName"/id_rsa.pub
 	_cpDiff "$scriptLocal"/ssh/known_hosts ~/.ssh/"$ubiquitiousBashID"/"$netName"/known_hosts
 	
 	_cpDiff "$scriptAbsoluteLocation" ~/.ssh/"$ubiquitiousBashID"/"$netName"/cautossh
 	_cpDiff "$scriptLocal"/ssh/ops ~/.ssh/"$ubiquitiousBashID"/"$netName"/ops
+	
+	_setup_ssh_extra
 	
 	return 0
 	
