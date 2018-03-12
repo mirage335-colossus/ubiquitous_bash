6ea974d01f884fba4bddfa24c59d8895f6cefdb5
mirage335 <spamfreemirage335@gmail.com>
2018-03-12 00:53:34 -0400

    Compile.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -2219,7 +2219,7 @@ _detect_x11_displays() {
 	do
 		if _permit_x11 env DISPLAY=:"$current_DISPLAY" XAUTHORITY="$current_XAUTH" xset -q > /dev/null 2>&1
 		then
-			export destination_AUTH="$currentAUTH"
+			export destination_AUTH="$current_XAUTH"
 			export destination_DISPLAY=":""$current_DISPLAY"
 			return 0
 		fi
@@ -2265,21 +2265,22 @@ _detect_x11() {
 		return 0
 	fi
 	
-	#destination_XAUTH=""
-	#_detect_x11_displays "$destination_XAUTH" && return 0
+	#export destination_AUTH=""
+	#_detect_x11_displays "$destination_AUTH" && return 0
 	
-	destination_XAUTH="$XAUTHORITY"
-	_detect_x11_displays "$destination_XAUTH" && return 0
+	export destination_AUTH="$XAUTHORITY"
+	_detect_x11_displays "$destination_AUTH" && return 0
 	
-	destination_XAUTH="$HOME"/.Xauthority
-	[[ -e "$destination_XAUTH" ]] && _detect_x11_displays "$destination_XAUTH" && return 0
+	export destination_AUTH="$HOME"/.Xauthority
+	[[ -e "$destination_AUTH" ]] && _detect_x11_displays "$destination_AUTH" && return 0
 	
-	local destination_XAUTH_x11vnc
-	_wantDep x11vnc && export destination_DISPLAY=$(x11vnc -findauth -finddpy | cut -f1 -d\, | cut -f2- -d\=) &&  destination_XAUTH_x11vnc=$(x11vnc -display "$destination_DISPLAY" -findauth | cut -f2- -d\=)
-	[[ -e "$destination_XAUTH_x11vnc" ]] && export destination_AUTH="$destination_XAUTH_x11vnc" && return 0
+	export destination_AUTH=$(ps -eo args -fp $(pgrep Xorg | head -n 1) 2>/dev/null | tail -n+2 | sort | sed 's/.*X.*\-auth\ \(.*\)/\1/' | sed 's/\ \-.*//g')
+	[[ -e "$destination_AUTH" ]] && _detect_x11_displays "$destination_AUTH" && return 0
 	
-	destination_XAUTH=$(ps -eo args -fp $(pgrep Xorg | head -n 1) 2>/dev/null | tail -n+2 | sort | sed 's/.*X.*\-auth\ \(.*\)/\1/' | sed 's/\ \-.*//g')
-	[[ -e "$destination_XAUTH" ]] && _detect_x11_displays "$destination_XAUTH" && return 0
+	#Unreliable, extra dependencies, last resort.
+	local destination_AUTH_x11vnc
+	_wantDep x11vnc && export destination_DISPLAY=$(x11vnc -findauth -finddpy | cut -f1 -d\, | cut -f2- -d\=) &&  destination_AUTH_x11vnc=$(x11vnc -display "$destination_DISPLAY" -findauth | cut -f2- -d\=)
+	[[ -e "$destination_AUTH_x11vnc" ]] && export destination_AUTH="$destination_AUTH_x11vnc" && return 0
 	
 	export destination_AUTH=""
 	export destination_DISPLAY=":0"
