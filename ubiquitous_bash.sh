779c7a60f89dc41196b0f6af0ff359e6247cf666
mirage335 <spamfreemirage335@gmail.com>
2018-03-16 21:18:33 -0400

    More arbitrary bup shorctuts. Log shortcut for bup.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -6682,29 +6682,44 @@ _bupNew() {
 	bup init
 }
 
+_bupLog() {
+	export BUP_DIR="./.bup"
+	
+	[[ ! -e "$BUP_DIR" ]] && return 1
+	git --git-dir=./.bup log
+}
+
 _bupList() {
 	export BUP_DIR="./.bup"
-	bup ls HEAD
+	
+	if [[ "$1" == "" ]]
+	then
+		bup ls "HEAD"
+		return
+	fi
+	[[ "$1" != "" ]] && bup ls "$@"
 }
 
 _bupStore() {
 	export BUP_DIR="./.bup"
 	
-	export BUP_DESTINATION="HEAD"
-	[[ "$1" != "" ]] && export BUP_DESTINATION="$1"
-	
-	[[ ! -e "$BUP_DIR" ]] && return 1
-	tar --exclude "$BUP_DIR" -cvf - . | bup split -n "$BUP_DESTINATION" -vv
+	if [[ "$1" == "" ]]
+	then
+		tar --exclude "$BUP_DIR" -cvf - . | bup split -n "HEAD" -vv
+		return
+	fi
+	[[ "$1" != "" ]] && tar --exclude "$BUP_DIR" -cvf - . | bup split -n "$@" -vv
 }
 
 _bupRetrieve() {
 	export BUP_DIR="./.bup"
 	
-	export BUP_SOURCE="HEAD"
-	[[ "$1" != "" ]] && export BUP_SOURCE="$1"
-	
-	[[ ! -e "$BUP_DIR" ]] && return 1
-	bup join "$BUP_SOURCE" | tar -xf -
+	if [[ "$1" == "" ]]
+	then
+		bup join "HEAD" | tar -xf -
+		return
+	fi
+	[[ "$1" != "" ]] && bup join "$@" | tar -xf -
 }
 
 _here_mkboot_grubcfg() {
