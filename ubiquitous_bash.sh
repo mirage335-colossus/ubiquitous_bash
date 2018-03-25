d93fdf6122582fdd5e4901e671d6f9e9a2e681ec
mirage335 <spamfreemirage335@gmail.com>
2018-03-25 19:20:18 -0400

    Process management for synergy.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -9201,6 +9201,9 @@ _findPort_synergy() {
 _prepare_synergy() {
 	export synergyPort=$(_findPort_synergy)
 	_messagePlain_probe 'synergyPort= '"$synergyPort"
+	
+	export synergyPIDfile="$safeTmpSSH"/.synpid
+	export synergyPIDfile_local="$safeTmp"/.synpid
 }
 
 _synergy_command_client() {
@@ -9212,7 +9215,7 @@ _synergy_command_client() {
 	mkdir -p "$HOME"
 	cd "$HOME"
 	
-	synergyc "$@"
+	synergyc --no-daemon "$@"
 }
 
 _synergy_command_server() {
@@ -9224,7 +9227,10 @@ _synergy_command_server() {
 	mkdir -p "$HOME"
 	cd "$HOME"
 	
-	synergy "$@"
+	pgrep ^synergy$ && return 0
+	
+	synergy "$@" &
+	[[ "$synergyPIDfile" != "" ]] && echo $! > "$synergyPIDfile"
 }
 
 _synergyc_operations() {
@@ -9240,7 +9246,7 @@ _synergyc_operations() {
 	
 	_messagePlain_nominal 'Launching synergy (client).'
 	
-	_synergy_command_client localhost:"$synergyPort"
+	_synergy_command_client --no-restart localhost:"$synergyPort"
 }
 
 _synergyc() {
@@ -9263,6 +9269,58 @@ _synergys_operations() {
 	_synergy_command_server "$@"
 }
 
+#No production use.
+_synergys_terminate() {
+	if [[ -e "$synergyPIDfile" ]] && [[ -s "$synergyPIDfile" ]]
+	then
+		_messagePlain_good 'found: usable "$synergyPIDfile"'
+		
+		pkill -P $(cat "$synergyPIDfile")
+		kill $(cat "$synergyPIDfile")
+		#sleep 1
+		#kill -KILL $(cat "$synergyPIDfile")
+		rm "$synergyPIDfile"
+		
+		pgrep "^synergy$" && _messagePlain_warn 'found: synergy process'
+		
+		return 0
+	fi
+	
+	_messagePlain_bad 'missing: usable "$synergyPIDfile'
+	_messagePlain_bad 'terminate: ^synergy$'
+	
+	pkill ^synergy$
+	
+	return 1
+}
+
+#No production use.
+_synergyc_terminate() {
+	if [[ -e "$synergyPIDfile" ]] && [[ -s "$synergyPIDfile" ]]
+	then
+		_messagePlain_good 'found: usable "$synergyPIDfile"'
+		
+		pkill -P $(cat "$synergyPIDfile")
+		kill $(cat "$synergyPIDfile")
+		#sleep 1
+		#kill -KILL $(cat "$synergyPIDfile")
+		rm "$synergyPIDfile"
+		
+		pgrep "^synergyc$" && _messagePlain_warn 'found: synergyc process'
+		
+		return 0
+	fi
+	
+	_messagePlain_bad 'missing: usable "$synergyPIDfile'
+	_messagePlain_bad 'terminate: ^synergyc$'
+	
+	pkill ^synergyc$
+	
+	rm "$synergyPIDfile"
+	
+	return 1
+}
+
 _synergys() {
 	"$scriptAbsoluteLocation" _synergys_operations "$@"
 }
@@ -9280,6 +9338,18 @@ _synergy_sequence() {
 	
 	_messageNormal 'synergy_sequence: Ready: _waitPort localhost synergyport= '"$synergyPort"
 	
+	#Service may not always be ready when port is up.
+	
+	sleep 0.8
+	if ! _checkPort localhost "$vncPort"
+	then
+		stty echo > /dev/null 2>&1
+		_stop_safeTmp_ssh "$@"
+		_stop
+	fi
+	
+	_messageNormal '_synergy_sequence: Ready: sleep, _checkPort. Launch: _synergyc'
+	
 	bash -c 'env synergyPort='"$synergyPort"' destination_DISPLAY='"$DISPLAY"' destination_AUTH='"$XAUTHORITY"' '"$scriptAbsoluteLocation"' _synergyc'
 	
 	_stop_safeTmp_ssh "$@"
@@ -9287,30 +9357,42 @@ _synergy_sequence() {
 }
 
 _synergy() {
-	"scriptAbsoluteLocation" _synergy_sequence "$@"
+	"$scriptAbsoluteLocation" _synergy_sequence "$@"
 }
 
 _push_synergy_sequence() {
-	_messageNormal '_push_synergy_sequence Start'
+	_messageNormal '_synergy_sequence Start'
 	_start
 	_start_safeTmp_ssh "$@"
 	_prepare_synergy
 	
-	_messageNormal '_push_synergy_sequence Launch: _synergys'
-	_synergy_ssh -L "$synergyPort":localhost:24800 "$@" 'env synergyPort='"$synergyPort"' '"$safeTmpSSH"/cautossh' _synergyc' &
+	_messageNormal '_synergy_sequence Launch: _synergys'
+	bash -c 'env synergyPort='"$synergyPort"' destination_DISPLAY='"$DISPLAY"' destination_AUTH='"$XAUTHORITY"' '"$scriptAbsoluteLocation"' _synergys' &
 	
 	_waitPort localhost "$synergyPort"
 	
 	_messageNormal 'synergy_sequence: Ready: _waitPort localhost synergyport= '"$synergyPort"
 	
-	bash -c 'env synergyPort='"$synergyPort"' destination_DISPLAY='"$DISPLAY"' destination_AUTH='"$XAUTHORITY"' '"$scriptAbsoluteLocation"' _synergys'
+	#Service may not always be ready when port is up.
+	
+	sleep 0.8
+	if ! _checkPort localhost "$vncPort"
+	then
+		stty echo > /dev/null 2>&1
+		_stop_safeTmp_ssh "$@"
+		_stop
+	fi
+	
+	_messageNormal '_synergy_sequence: Ready: sleep, _checkPort. Launch: _synergyc'
+	
+	_synergy_ssh -L "$synergyPort":localhost:24800 "$@" 'env synergyPort='"$synergyPort"' '"$safeTmpSSH"/cautossh' _synergyc'
 	
 	_stop_safeTmp_ssh "$@"
 	_stop
 }
 
 _push_synergy() {
-	"scriptAbsoluteLocation" _push_synergy_sequence "$@"
+	"$scriptAbsoluteLocation" _push_synergy_sequence "$@"
 }
 
 _x220_vgaSmall() {
