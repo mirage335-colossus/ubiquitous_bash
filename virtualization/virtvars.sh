aeae58c7097b428e64f01f54cc9facc09e7f8435
mirage335 <spamfreemirage335@gmail.com>
2018-03-28 16:43:08 -0400

    Regression - correct memory assignment.

--- virtualization/virtvars.sh
+++ virtualization/virtvars.sh
@@ -38,13 +38,16 @@ _vars_vmMemoryAllocationDefault() {
 	export vmMemoryAllocationDefault=96
 	
 	[[ "$hostMemoryQuantity" -lt "500000" ]] && export vmMemoryAllocationDefault=256 && return 1
-	[[ "$hostMemoryQuantity" -lt "800000" ]] && export vmMemoryAllocationDefault=512 && return 1
-	#[[ "$hostMemoryQuantity" -lt "1500000" ]] && export vmMemoryAllocationDefault=512 && return 1
-	#[[ "$hostMemoryQuantity" -lt "3000000" ]] && export vmMemoryAllocationDefault=896 && return 1
-	[[ "$hostMemoryQuantity" -lt "6000000" ]] && export vmMemoryAllocationDefault=1256 && return 1
-	#[[ "$hostMemoryQuantity" -lt "8000000" ]] && export vmMemoryAllocationDefault=1256 && return 1
-	[[ "$hostMemoryQuantity" -lt "12000000" ]] && export vmMemoryAllocationDefault=1512 && return 1
-	#[[ "$hostMemoryQuantity" -lt "16000000" ]] && export vmMemoryAllocationDefault=1512 && return 1
+	
+	[[ "$hostMemoryQuantity" -lt "800000" ]] && export vmMemoryAllocationDefault=512 && return 0
+	#[[ "$hostMemoryQuantity" -lt "1500000" ]] && export vmMemoryAllocationDefault=512 && return 0
+	#[[ "$hostMemoryQuantity" -lt "3000000" ]] && export vmMemoryAllocationDefault=896 && return 0
+	[[ "$hostMemoryQuantity" -lt "6000000" ]] && export vmMemoryAllocationDefault=1256 && return 0
+	#[[ "$hostMemoryQuantity" -lt "8000000" ]] && export vmMemoryAllocationDefault=1256 && return 0
+	[[ "$hostMemoryQuantity" -lt "12000000" ]] && export vmMemoryAllocationDefault=1512 && return 0
+	#[[ "$hostMemoryQuantity" -lt "16000000" ]] && export vmMemoryAllocationDefault=1512 && return 0
+	
+	[[ "$hostMemoryQuantity" -ge "16000000" ]] && export vmMemoryAllocationDefault=1512 && return 0
 	
 }
 
