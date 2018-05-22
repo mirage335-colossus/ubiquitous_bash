753231c19043b7d84de4bd9b2e61692724606990
mirage335 <spamfreemirage335@gmail.com>
2018-05-22 19:48:53 -0400

    Modeling.

--- _lib/kit/containment/_prog/core.sh
+++ _lib/kit/containment/_prog/core.sh
@@ -1,21 +1,26 @@
-##### Core
+#all2=""
 
+#command
 _app_command() {
 	"$scriptAbsoluteFolder"/_local/setups/appExe "$@"
 }
 
+#edit
 _app_edit() {
 	"$scriptAbsoluteLocation" _editFakeHome "$scriptAbsoluteLocation" _app_command "$@"
 }
 
+#user
 _app_user() {
 	"$scriptAbsoluteLocation" _userFakeHome "$scriptAbsoluteLocation" _app_command "$@"
 }
 
+#virtualized
 _v_app() {
 	_userQemu "$scriptAbsoluteLocation" _app_user "$@"
 }
 
+#default
 _app() {
 	if ! _check_prog
 	then
@@ -23,13 +28,16 @@ _app() {
 		_v${FUNCNAME[0]} "$@"
 		return
 	fi
-	
 	_app_user "$@" && return 0
-	
+
 	#_messageNormal 'Launch: _v'${FUNCNAME[0]}
 	#_v${FUNCNAME[0]} "$@"
 }
 
+#duplicate _anchor
 _refresh_anchors() {
 	cp -a "$scriptAbsoluteFolder"/_anchor "$scriptAbsoluteFolder"/_app
 }
+
+
+#####^ Core
