23f30cdd7673da8604b649ffedcf19196948980a
mirage335 <spamfreemirage335@gmail.com>
2018-05-27 11:03:50 -0400

    Kit documentation.

---
+++ _lib/kit/containment/_prog/generate_bash_prog.sh
@@ -0,0 +1,15 @@
+_generate_compile_bash_prog() {
+	"$scriptAbsoluteLocation" _true
+	
+	rm "$scriptAbsoluteFolder"/ubiquitous_bash.sh
+	
+	#"$scriptAbsoluteLocation" _compile_bash cautossh cautossh
+	#"$scriptAbsoluteLocation" _compile_bash lean lean.sh
+	
+	"$scriptAbsoluteLocation" _compile_bash core ubiquitous_bash.sh
+	
+	#"$scriptAbsoluteLocation" _compile_bash "" ""
+	#"$scriptAbsoluteLocation" _compile_bash ubiquitous_bash ubiquitous_bash.sh
+	
+	#"$scriptAbsoluteLocation" _package
+}
