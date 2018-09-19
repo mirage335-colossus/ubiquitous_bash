48bebb8ebe4f28be50243e609a2f04999de1f053
mirage335 <spamfreemirage335@gmail.com>
2018-09-19 01:46:48 -0400

    Untracked.

---
+++ generic/filesystem/allLogic.sh
@@ -0,0 +1,16 @@
+_all_exist() {
+	local currentArg
+	for currentArg in "$@"
+	do
+		! [[ -e "$currentArg" ]] && return 1
+	done
+	
+	return 0
+}
+
+_wait_not_all_exist() {
+	while ! _all_exist "$@"
+	do
+		sleep 0.1
+	done
+}
