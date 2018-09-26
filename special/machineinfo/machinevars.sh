5830ca8b7503b5e1b1326ae68b2bbfe814b0928a
mirage335 <spamfreemirage335@gmail.com>
2018-09-26 16:54:00 -0400

    Default hostMemory in absence of meminfo.

--- special/machineinfo/machinevars.sh
+++ special/machineinfo/machinevars.sh
@@ -1,4 +1,12 @@
 #Machine information.
-export hostMemoryTotal=$(cat /proc/meminfo | grep MemTotal | tr -cd '[[:digit:]]')
-export hostMemoryAvailable=$(cat /proc/meminfo | grep MemAvailable | tr -cd '[[:digit:]]')
-export hostMemoryQuantity="$hostMemoryTotal"
+
+if [[ -e "/proc/meminfo" ]]
+then
+	export hostMemoryTotal=$(cat /proc/meminfo | grep MemTotal | tr -cd '[[:digit:]]')
+	export hostMemoryAvailable=$(cat /proc/meminfo | grep MemAvailable | tr -cd '[[:digit:]]')
+	export hostMemoryQuantity="$hostMemoryTotal"
+else
+	export hostMemoryTotal="384000"
+	export hostMemoryAvailable="256000"
+	export hostMemoryQuantity="$hostMemoryTotal"
+fi
--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -12854,9 +12854,17 @@ export globalBuildTmp="$globalBuildDir"/tmp
 export tmpPrefix=""
 
 #Machine information.
-export hostMemoryTotal=$(cat /proc/meminfo | grep MemTotal | tr -cd '[[:digit:]]')
-export hostMemoryAvailable=$(cat /proc/meminfo | grep MemAvailable | tr -cd '[[:digit:]]')
-export hostMemoryQuantity="$hostMemoryTotal"
+
+if [[ -e "/proc/meminfo" ]]
+then
+	export hostMemoryTotal=$(cat /proc/meminfo | grep MemTotal | tr -cd '[[:digit:]]')
+	export hostMemoryAvailable=$(cat /proc/meminfo | grep MemAvailable | tr -cd '[[:digit:]]')
+	export hostMemoryQuantity="$hostMemoryTotal"
+else
+	export hostMemoryTotal="384000"
+	export hostMemoryAvailable="256000"
+	export hostMemoryQuantity="$hostMemoryTotal"
+fi
 
 export virtGuestUserDrop="ubvrtusr"
 export virtGuestUser="$virtGuestUserDrop"
