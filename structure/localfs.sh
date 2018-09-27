fde7b34dc927ee5416cc570b9dd8011d7c4c0552
mirage335 <spamfreemirage335@gmail.com>
2018-09-27 17:01:11 -0400

    Relax unnecessary chown requirements.

--- structure/localfs.sh
+++ structure/localfs.sh
@@ -12,12 +12,19 @@ _prepare_abstract() {
 	! mkdir -p "$abstractfs_root" && exit 1
 	chmod 0700 "$abstractfs_root" > /dev/null 2>&1
 	! chmod 700 "$abstractfs_root" && exit 1
-	! chown "$USER":"$USER" "$abstractfs_root" && exit 1
+	if ! chown "$USER":"$USER" "$abstractfs_root"
+	then
+		! /sbin/chown "$USER" "$abstractfs_root" && exit 1
+	fi
+	
 	
 	! mkdir -p "$abstractfs_lock" && exit 1
 	chmod 0700 "$abstractfs_lock" > /dev/null 2>&1
 	! chmod 700 "$abstractfs_lock" && exit 1
-	! chown "$USER":"$USER" "$abstractfs_lock" && exit 1
+	if ! chown "$USER":"$USER" "$abstractfs_lock"
+	then
+		! /sbin/chown "$USER" "$abstractfs_root" && exit 1
+	fi
 }
 
 _prepare() {
