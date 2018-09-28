241086e82e2d4056b6d42decebb30ad07431cd46
mirage335 <spamfreemirage335@gmail.com>
2018-09-27 22:16:56 -0400

    Silence unaccommodated messages.

--- generic/filesystem/relink.sh
+++ generic/filesystem/relink.sh
@@ -34,9 +34,9 @@ _relink() {
 _relink_relative() {
 	export relinkRelativeUb=""
 	
-	[[ "$relinkRelativeUb" != "true" ]] && ln --help 2>/dev/null | grep '\-\-relative' && export relinkRelativeUb="true"
-	[[ "$relinkRelativeUb" != "true" ]] && ln 2>&1 | grep '\-\-relative' > /dev/null && export relinkRelativeUb="true"
-	[[ "$relinkRelativeUb" != "true" ]] && man ln 2>/dev/null | grep '\-\-relative' && export relinkRelativeUb="true"
+	[[ "$relinkRelativeUb" != "true" ]] && ln --help 2>/dev/null | grep '\-\-relative' > /dev/null 2>&1 && export relinkRelativeUb="true"
+	[[ "$relinkRelativeUb" != "true" ]] && ln 2>&1 | grep '\-\-relative' > /dev/null 2>&1 && export relinkRelativeUb="true"
+	[[ "$relinkRelativeUb" != "true" ]] && man ln 2>/dev/null | grep '\-\-relative' > /dev/null 2>&1 && export relinkRelativeUb="true"
 	
 	_relink_procedure "$@"
 	export relinkRelativeUb=""
