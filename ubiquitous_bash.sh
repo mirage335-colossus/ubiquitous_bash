bc19ef1f89522c714db9deae8fe35c3c0d384afc
mirage335 <spamfreemirage335@gmail.com>
2018-11-01 20:19:30 -0400

    Correct port directory assignment.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -2391,16 +2391,16 @@ _torServer_SSH_writeCfg() {
 	[[ "$currentLocalSSHport" == "" ]] && currentLocalSSHport="$LOCALSSHPORT"
 	[[ "$currentLocalSSHport" == "" ]] && currentLocalSSHport=22
 	
-	mkdir -p "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"
+	mkdir -p "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"
 	
-	rm "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}" > /dev/null 2>&1
+	rm "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}" > /dev/null 2>&1
 	
-	echo "RunAsDaemon 0" >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
-	echo "DataDirectory "'"'"$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"'"' >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
-	echo  >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
+	echo "RunAsDaemon 0" >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
+	echo "DataDirectory "'"'"$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"'"' >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
+	echo  >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
 	
-	echo "SocksPort 0" >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
-	echo  >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
+	echo "SocksPort 0" >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
+	echo  >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
 	
 	local currentReversePort
 	for currentReversePort in "${matchingReversePorts[@]}"
@@ -2409,11 +2409,11 @@ _torServer_SSH_writeCfg() {
 		mkdir -p "$scriptLocal"/tor/sshd/"$currentReversePort"
 		chmod 700 "$scriptLocal"/tor/sshd/"$currentReversePort"
 		
-		echo "HiddenServiceDir "'"'"$scriptLocal"/tor/sshd/"$currentReversePort"/'"' >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
+		echo "HiddenServiceDir "'"'"$scriptLocal"/tor/sshd/"$currentReversePort"/'"' >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
 		
-		echo "HiddenServicePort ""$currentReversePort"" 127.0.0.1:""$currentLocalSSHport" >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
+		echo "HiddenServicePort ""$currentReversePort"" 127.0.0.1:""$currentLocalSSHport" >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
 		
-		echo  >> "$scriptLocal"/tor/sshd/dd_"${matchingOffsetPorts[0]}"/torrc_"${matchingOffsetPorts[0]}"
+		echo  >> "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
 		
 	done
 	
@@ -2499,7 +2499,7 @@ _torServer_SSH_launch() {
 	
 	_torServer_SSH_writeCfg
 	
-	tor -f "$scriptLocal"/tor/sshd/dd/torrc_"${matchingOffsetPorts[0]}"
+	tor -f "$scriptLocal"/tor/sshd/dd_"${matchingReversePorts[0]}"/torrc_"${matchingReversePorts[0]}"
 	
 	_show_torServer_SSH_hostnames
 	
