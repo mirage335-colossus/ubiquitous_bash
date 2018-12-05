9c0c3da3c4ca2de24fe49103cab8dd90fb523ca7
mirage335 <spamfreemirage335@gmail.com>
2018-12-04 22:59:20 -0500

    Allow native TigerVNC override under cygwin.

--- lean.sh
+++ lean.sh
@@ -179,19 +179,19 @@ fi
 
 #if ! type vncviewer > /dev/null 2>&1 && type '/cygdrive/c/Program Files/TigerVNC/vncviewer.exe' > /dev/null 2>&1
 
-#if type '/cygdrive/c/Program Files/TigerVNC/vncviewer.exe' > /dev/null 2>&1 && uname -a | grep -i cygwin > /dev/null 2>&1
-#then
-#	vncviewer() {
-#		'/cygdrive/c/Program Files/TigerVNC/vncviewer.exe' "$@"
-#	}
-#fi
+if type '/cygdrive/c/Program Files/TigerVNC/vncviewer.exe' > /dev/null 2>&1 && uname -a | grep -i cygwin > /dev/null 2>&1
+then
+	vncviewer() {
+		'/cygdrive/c/Program Files/TigerVNC/vncviewer.exe' "$@"
+	}
+fi
 
-#if type '/cygdrive/c/Program Files (x86)/TigerVNC/vncviewer.exe' > /dev/null 2>&1 && uname -a | grep -i cygwin > /dev/null 2>&1
-#then
-#	vncviewer() {
-#		'/cygdrive/c/Program Files (x86)/TigerVNC/vncviewer.exe' "$@"
-#	}
-#fi
+if type '/cygdrive/c/Program Files (x86)/TigerVNC/vncviewer.exe' > /dev/null 2>&1 && uname -a | grep -i cygwin > /dev/null 2>&1
+then
+	vncviewer() {
+		'/cygdrive/c/Program Files (x86)/TigerVNC/vncviewer.exe' "$@"
+	}
+fi
 
 
 #####Utilities
