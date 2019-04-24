9e6f00eea513976fdf8716d29d97d918fb66c119
mirage335 <spamfreemirage335@gmail.com>
2019-04-23 20:01:05 -0400

    Documentation.

--- _config/_config/netvars.sh
+++ _config/_config/netvars.sh
@@ -27,7 +27,7 @@ _offset_reversePorts() {
 
 # WARNING Must use unique netName!
 export netName=default
-export gatewayName=gw-"$netName"
+export gatewayName=gw-"$netName"-"$netName"
 export LOCALSSHPORT=22
 
 
