9aacbbba79feb8c74e2acd0e7afd332522218237
mirage335 <spamfreemirage335@gmail.com>
2019-06-24 17:26:45 -0400

    Draft raspi PTUUID automatic replacement.

--- shortcuts/image/gparted.sh
+++ shortcuts/image/gparted.sh
@@ -3,18 +3,59 @@ _test_gparted() {
 }
 
 _gparted_sequence() {
+	_messageNormal 'launch gparted'
+	
 	_start
 	
 	_mustGetSudo
 	
-	_openLoop || _stop 1
+	_messagePlain_nominal 'Attempt: _openLoop'
+	! _openLoop && _messageFAIL
+	
+	
+	_messagePlain_nominal 'Launch: gparted'
 	
 	local imagedev
 	imagedev=$(cat "$scriptLocal"/imagedev)
 	
+	_messagePlain_probe 'blkid'
+	sudo -n blkid "$imagedev"
+	local orig_ptuuid
+	orig_ptuuid=$(sudo blkid -s PTUUID -o value /dev/loop0)
+	
 	sudo -n gparted "$imagedev"
 	
-	_closeLoop || _stop 1
+	_messagePlain_probe 'blkid'
+	sudo -n blkid "$imagedev"
+	local modified_ptuuid
+	modified_ptuuid=$(sudo blkid -s PTUUID -o value /dev/loop0)
+	
+	
+	_messagePlain_nominal 'Attempt: _closeLoop'
+	! _closeLoop && _messageFAIL
+	
+	
+	if [[ -e "$scriptLocal"/vm-raspbian.img ]]
+	then
+		_messageNormal 'Config: raspbian'
+		_messagePlain_probe_var orig_ptuuid
+		_messagePlain_probe_var modified_ptuuid
+		
+		_messagePlain_nominal 'Attempt: _openChRoot'
+		! _openChRoot && _messageFAIL
+		
+		
+		_messagePlain_nominal 'Replace: /boot/cmdline.txt'
+		_messagePlain_probe "$globalVirtFS"/../boot/cmdline.txt
+		sed -i 's/'"$orig_ptuuid"'/'"$modified_ptuuid"'/g' "$globalVirtFS"/../boot/cmdline.txt
+		
+		_messagePlain_nominal 'Replace: /etc/fstab'
+		sed -i 's/'"$orig_ptuuid"'/'"$modified_ptuuid"'/g' "$globalVirtFS"/../etc/fstab
+		
+		
+		_messagePlain_nominal 'Attempt: _closeChRoot'
+		! _closeChRoot && _messageFAIL
+	fi
 	
 	_stop
 }
