3c5c505a579c7084b12cd008b268c58f4a3f4b5e
mirage335 <spamfreemirage335@gmail.com>
2020-01-05 05:36:02 -0500

    Accommodate VirtualBox new version.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -10356,11 +10356,13 @@ _set_instance_vbox_features() {
 	
 	_messagePlain_nominal "Setting VBox VM features."
 	
-	if ! _messagePlain_probe_cmd VBoxManage modifyvm "$sessionid" --biosbootmenu disabled --bioslogofadein off --bioslogofadeout off --bioslogodisplaytime 1 --vram 128 --memory "$vmMemoryAllocation" --nic1 "$vboxNic" --nictype1 "$vboxNictype" --clipboard bidirectional --accelerate3d off --accelerate2dvideo off --vrde off --audio pulse --usb on --cpus "$vboxCPUs" --ioapic on --acpi on --pae on --chipset "$vboxChipset" --audiocontroller="$vboxAudioController"
+	if ! _messagePlain_probe_cmd VBoxManage modifyvm "$sessionid" --biosbootmenu disabled --bioslogofadein off --bioslogofadeout off --bioslogodisplaytime 1 --vram 128 --memory "$vmMemoryAllocation" --nic1 "$vboxNic" --nictype1 "$vboxNictype" --accelerate3d off --accelerate2dvideo off --vrde off --audio pulse --usb on --cpus "$vboxCPUs" --ioapic on --acpi on --pae on --chipset "$vboxChipset" --audiocontroller="$vboxAudioController"
 	then
 		_messagePlain_bad 'fail: VBoxManage'
 		return 1
 	fi
+	
+	#_messagePlain_probe_cmd VBoxManage controlvm "$sessionid" clipboard bidirectional
 	
 	# Linux hosts may benefit from 'vboxsvga' instead of 'vmsvga'.
 	#https://wiki.gentoo.org/wiki/VirtualBox
