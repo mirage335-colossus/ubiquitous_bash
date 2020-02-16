d440cd565c681828ce34428602e0de183273b925
mirage335 <spamfreemirage335@gmail.com>
2020-02-15 20:00:53 -0500

    Correct binfmts test.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -9552,13 +9552,16 @@ _testQEMU_x64-raspi() {
 	
 	! _testQEMU_hostArch_x64-raspi && echo "warn: not checking x64 translation" && return 0
 	
-	if ! sudo -n /usr/sbin/update-binfmts --display | grep qemu > /dev/null 2>&1
+	
+	
+	
+	if ! sudo -n cat /proc/sys/fs/binfmt_misc/* 2> /dev/null | grep qemu | grep 'arm$\|arm-static$' > /dev/null 2>&1
 	then
 		echo 'binfmts does not mention qemu-arm'
 		_stop 1
 	fi
 	
-	if ! sudo -n /usr/sbin/update-binfmts --display | grep qemu-armeb-static > /dev/null 2>&1
+	if ! sudo -n cat /proc/sys/fs/binfmt_misc/* 2> /dev/null | grep qemu | grep 'armeb$\|armeb-static$' > /dev/null 2>&1
 	then
 		echo 'binfmts does not mention qemu-armeb'
 		_stop 1
