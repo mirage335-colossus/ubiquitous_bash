35147a23219dcb82d23c516627eeab12d8b27151
mirage335 <spamfreemirage335@gmail.com>
2020-05-06 02:23:11 -0400

    Documentation.

--- virtualization/abstractfs/abstractfsvars.sh
+++ virtualization/abstractfs/abstractfsvars.sh
@@ -68,8 +68,13 @@ _set_share_abstractfs_reset() {
 _set_share_abstractfs() {
 	_set_share_abstractfs_reset
 	
+	# ATTENTION: Using absolute folder, may preserve apparent parent directory name at the expense of reducing likelihood of 8.3 compatibility.
+	#./ubiquitous_bash.sh _abstractfs ls -lad ./.
+	#/dev/shm/uk4u/randomid/.
+	#/dev/shm/uk4u/randomid/ubiquitous_bash
 	export sharedHostProjectDir="$abstractfs_base"
 	#export sharedHostProjectDir=$(_getAbsoluteFolder "$abstractfs_base")
+	
 	export sharedGuestProjectDir="$abstractfs"
 	
 	#Blank default. Resolves to lowest directory shared by "$PWD" and "$@" .
