16b5dc7954738b0bf05a1e219d19cc86cb2cf63f
mirage335 <spamfreemirage335@gmail.com>
2020-09-08 23:38:55 -0400

    Draft - improved systemd shutdown hook.

--- os/unix/systemd/here_systemd.sh
+++ os/unix/systemd/here_systemd.sh
@@ -1,11 +1,13 @@
 #https://unix.stackexchange.com/questions/39226/how-to-run-a-script-with-systemd-right-before-shutdown
-
+# In theory, 'sleep 1892160000' should create a process that will run for at least 60 years, with 'sleep' binaries that support 'floating point' numbers of seconds, which should be tested for by timetest. This should not be depended upon unless necessary however.
 
 _here_systemd_shutdown_action() {
 
 cat << 'CZXWXcRMTo8EmM8i4d'
 [Unit]
-Description=...
+Description=$ubiquitiousBashID-_here_systemd_shutdown_action
+DefaultDependencies=no
+Before=shutdown.target reboot.target halt.target
 
 [Service]
 Type=oneshot
@@ -27,11 +29,14 @@ _here_systemd_shutdown() {
 
 cat << 'CZXWXcRMTo8EmM8i4d'
 [Unit]
-Description=...
+Description=$ubiquitiousBashID-_here_systemd_shutdown_action
+DefaultDependencies=no
+Before=shutdown.target reboot.target halt.target
 
 [Service]
 Type=oneshot
 RemainAfterExit=true
+ExecStart=/bin/true
 CZXWXcRMTo8EmM8i4d
 
 echo ExecStop="$scriptAbsoluteLocation" _remoteSigTERM "$safeTmp"/.pid "$sessionid"
--- ubcore.sh
+++ ubcore.sh
@@ -3194,13 +3194,15 @@ _fetchDep_debian() {
 }
 
 #https://unix.stackexchange.com/questions/39226/how-to-run-a-script-with-systemd-right-before-shutdown
-
+# In theory, 'sleep 1892160000' should create a process that will run for at least 60 years, with 'sleep' binaries that support 'floating point' numbers of seconds, which should be tested for by timetest. This should not be depended upon unless necessary however.
 
 _here_systemd_shutdown_action() {
 
 cat << 'CZXWXcRMTo8EmM8i4d'
 [Unit]
-Description=...
+Description=$ubiquitiousBashID-_here_systemd_shutdown_action
+DefaultDependencies=no
+Before=shutdown.target reboot.target halt.target
 
 [Service]
 Type=oneshot
@@ -3222,11 +3224,14 @@ _here_systemd_shutdown() {
 
 cat << 'CZXWXcRMTo8EmM8i4d'
 [Unit]
-Description=...
+Description=$ubiquitiousBashID-_here_systemd_shutdown_action
+DefaultDependencies=no
+Before=shutdown.target reboot.target halt.target
 
 [Service]
 Type=oneshot
 RemainAfterExit=true
+ExecStart=/bin/true
 CZXWXcRMTo8EmM8i4d
 
 echo ExecStop="$scriptAbsoluteLocation" _remoteSigTERM "$safeTmp"/.pid "$sessionid"
