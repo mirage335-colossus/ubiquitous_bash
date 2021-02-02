d27d3a0236b8b73b5f6f7bccfb04158e38ed9785
mirage335 <spamfreemirage335@gmail.com>
2021-02-02 14:00:36 -0500

    Documentation.

--- compile.sh
+++ compile.sh
@@ -4807,6 +4807,7 @@ then
 	
 	# DANGER: Mechanism of workaround 'ub_trapSet_stop_emergency' is not fully understood, and was added undesirably late in development, with unknown effects. Nevertheless, a need for such functionality is expected to be encountered only rarely.
 	# At least '_closeChRoot' , '_userChRoot' , '_userVBox' do not seem to have lost functionality.
+	# DANGER: Any shell command matching ' _timeout.*&$ ' (backgrounding of _timeout) will probably be unable to reach '_stop' correctly, and may not remove temporary directories, etc.
 	if [[ "$ub_trapSet_stop_emergency" != 'true' ]]
 	then
 		trap 'excode=$?; _stop_emergency $excode; trap - EXIT; echo $excode' INT TERM	# reset
