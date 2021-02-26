88280a6754939dce3d649e24f6c25b95aa35a80b
mirage335 <spamfreemirage335@gmail.com>
2021-02-26 12:00:34 -0500

    Draft.

--- os/override/override.sh
+++ os/override/override.sh
@@ -86,3 +86,163 @@ _sleep_spinlock() {
 }
 
 
+
+# CAUTION: Do not alow similarity of this function name to other commonly used function names . Unintended tab completion could significantly and substantially impede user , particularly if 'disk' hibernation is not properly available .
+_____special_hibernate() {
+	! _mustGetSudo && exit 1
+	
+	sudo -n swapon /dev/disk/by-uuid/469457fc-293f-46ec-92da-27b5d0c36b17
+	
+	free -m
+	
+	sudo -n systemctl hibernate
+	
+	if sudo -n lsmod | grep vboxguest > /dev/null 2>&1
+	then
+		# Expected to result in longer delay if system is not idle.
+		local currentIterations
+		# 3.0s
+		currentIterations=0
+		while [[ "$currentIterations" -lt 6 ]]
+		do
+			let currentIterations="$currentIterations + 1"
+			sleep 0.5
+		done
+		# 1.0s (4.0s total)
+		currentIterations=0
+		while [[ "$currentIterations" -lt 10 ]]
+		do
+			let currentIterations="$currentIterations + 1"
+			sleep 0.1
+		done
+		# 0.15s (4.15s total)
+		currentIterations=0
+		while [[ "$currentIterations" -lt 15 ]]
+		do
+			let currentIterations="$currentIterations + 1"
+			sleep 0.01
+		done
+		
+		sudo -n pkill VBoxService
+		sudo -n pkill VBoxClient
+		sleep 2
+		sudo -n pkill -KILL VBoxService
+		sudo -n pkill -KILL VBoxClient
+		
+		sleep 1
+		sudo -n rmmod vboxvideo
+		sudo -n rmmod vboxguest
+		
+		sleep 0.1
+		sudo -n rmmod vboxvideo
+		sudo -n rmmod vboxguest
+		sleep 0.1
+		sudo -n modprobe vboxvideo
+		sudo -n modprobe vboxguest
+		
+		sleep 1
+		sudo -n VBoxService --pidfile /var/run/vboxadd-service.sh
+		
+		sleep 3
+		sudo -n VBoxClient-all
+	fi
+	
+	disown -a -h -r
+	disown -a -r
+	
+	return 0
+}
+
+# WARNING: Untested.
+# CAUTION: Do not alow similarity of this function name to other commonly used function names . Unintended tab completion could significantly and substantially impede user.
+_____special_dent_backup() {
+	! _mustGetSudo && exit 1
+	
+	sudo -n mkdir -p /mnt/dent
+	! mountpoint /mnt/dent && sudo -n mount -o ro /dev/disk/by-uuid/d82e3d89-3156-4484-bde2-ccc534ca440b /mnt/dent
+	! mountpoint /mnt/dent && exit 1
+	
+	
+	sudo -n mount -o remount,rw /mnt/dent
+	
+	
+	if type -p 'pigz' > /dev/null 2>&1
+	then
+		sudo -n dd if=/dev/disk/by-uuid/469457fc-293f-46ec-92da-27b5d0c36b17 bs=1M | pigz --fast > /mnt/dent/hint_bak.gz
+	elif type -p 'gzip' > /dev/null 2>&1
+	then
+		sudo -n dd if=/dev/disk/by-uuid/469457fc-293f-46ec-92da-27b5d0c36b17 bs=1M | gzip --fast > /mnt/dent/hint_bak.gz
+	else
+		sudo -n dd if=/dev/disk/by-uuid/469457fc-293f-46ec-92da-27b5d0c36b17 bs=1M > /mnt/dent/hint_bak
+	fi
+	
+	
+	sudo -n mkdir -p /mnt/bulk
+	! mountpoint /mnt/bulk && sudo -n mount -o ro /dev/disk/by-uuid/f1edb7fb-13b1-4c97-91d2-baf50e6d65d8
+	! mountpoint /mnt/bulk && exit 1
+	
+	
+	sudo -n mkdir -p /mnt/dent/bulk_bak
+	[[ ! -e /mnt/dent/bulk_bak ]] && exit 1
+	[[ ! -d /mnt/dent/bulk_bak ]] && exit 1
+	
+	sudo -n rsync -ax --delete /mnt/bulk/. /mnt/dent/bulk_bak/.
+	
+	
+	
+	
+	sudo -n mount -o remount,ro /mnt/dent
+	sync
+	
+	sudo -n umount /mnt/dent
+	sync
+	
+	return 0
+}
+
+# WARNING: Untested.
+# CAUTION: Do not alow similarity of this function name to other commonly used function names . Unintended tab completion could significantly and substantially impede user.
+# WARNING: By default does not restore contents of '/mnt/bulk' assuming simultaneous use of persistent storage and hibernation backup is sufficiently unlikely and risky that a request to the user is preferable.
+_____special_dent_restore() {
+	! _mustGetSudo && exit 1
+	
+	sudo -n mkdir -p /mnt/dent
+	! mountpoint /mnt/dent && sudo -n mount -o ro /dev/disk/by-uuid/d82e3d89-3156-4484-bde2-ccc534ca440b /mnt/dent
+	! mountpoint /mnt/dent && exit 1
+	#sudo -n mount -o remount,ro /mnt/dent
+	
+	
+	gzip -c /mnt/dent/hint_bak.gz | dd of=/dev/disk/by-uuid/469457fc-293f-46ec-92da-27b5d0c36b17 bs=1M
+	
+	
+	#sudo -n mkdir -p /mnt/bulk
+	#! mountpoint /mnt/bulk && sudo -n mount -o ro /dev/disk/by-uuid/f1edb7fb-13b1-4c97-91d2-baf50e6d65d8
+	#! mountpoint /mnt/bulk && exit 1
+	#sudo -n mount -o remount,rw /mnt/bulk
+	
+	
+	#sudo -n mkdir -p /mnt/dent/bulk_bak
+	#[[ ! -e /mnt/dent/bulk_bak ]] && exit 1
+	#[[ ! -d /mnt/dent/bulk_bak ]] && exit 1
+	
+	#sudo -n rsync -ax --delete /mnt/dent/bulk_bak/. /mnt/bulk/.
+	
+	
+	
+	
+	sudo -n mount -o remount,ro /mnt/dent
+	sync
+	sudo -n umount /mnt/dent
+	sync
+	
+	_messagePlain_request 'request: consider restoring /mnt/bulk (not overwritten by default)'
+	
+	return 0
+}
+
+
+
+
+
+
+
