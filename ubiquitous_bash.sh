6ccd7072986df7f301dfa689d45f2e39a9c30299
mirage335 <spamfreemirage335@gmail.com>
2021-04-26 17:38:07 -0400

    Documentation.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='241531999'
+export ub_setScriptChecksum_contents='876209317'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -16060,6 +16060,38 @@ _test_mktorrent() {
 #aws
 
 #google
+
+# https://github.com/tensorflow/cloud#cluster-and-distribution-strategy-configuration
+# https://www.tensorflow.org/api_docs/python/tf/distribute/OneDeviceStrategy
+# https://www.tensorflow.org/guide/distributed_training
+# https://colab.research.google.com/notebooks/intro.ipynb
+
+# https://github.com/tensorflow/cloud#setup-instructions
+
+#which gcloud
+
+#export PROJECT_ID=<your-project-id>
+#gcloud config set project $PROJECT_ID
+
+#export SA_NAME=<your-sa-name>
+#gcloud iam service-accounts create $SA_NAME
+#gcloud projects add-iam-policy-binding $PROJECT_ID --member serviceAccount:$SA_NAME@$PROJECT_ID.iam.gserviceaccount.com --role 'roles/editor'
+#gcloud iam service-accounts keys create ~/key.json --iam-account $SA_NAME@$PROJECT_ID.iam.gserviceaccount.com
+
+#export GOOGLE_APPLICATION_CREDENTIALS=~/key.json
+
+#BUCKET_NAME="your-bucket-name"
+#REGION="us-central1"
+#gcloud auth login
+#gsutil mb -l $REGION gs://$BUCKET_NAME
+
+#gcloud auth configure-docker
+
+
+
+
+
+
 
 #ibm
 
