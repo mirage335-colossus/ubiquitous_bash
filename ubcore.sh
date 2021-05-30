254104e19e2b32603a7267b42c1658c8f14208da
mirage335 <spamfreemirage335@gmail.com>
2021-05-29 23:06:40 -0400

    Correction.

--- ubcore.sh
+++ ubcore.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='4146997442'
+export ub_setScriptChecksum_contents='143594543'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -1028,7 +1028,7 @@ _setup_ubiquitousBash_cygwin_procedure() {
 
 
 _setup_ubiquitousBash_cygwin() {
-	"$scriptAbsoluteLocation" _setup_ubiquitousBash_cygwin "$@"
+	"$scriptAbsoluteLocation" _setup_ubiquitousBash_cygwin_procedure_root "$@"
 }
 
 
