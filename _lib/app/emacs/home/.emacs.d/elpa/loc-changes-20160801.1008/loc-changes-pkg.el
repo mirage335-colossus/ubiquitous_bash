71f1215b137c2db351ba62134fcf872195239ec3
mirage335 <spamfreemirage335@gmail.com>
2021-10-19 22:50:44 -0400

    WIP - move emacs home config to bundle from lib.

--- _lib/app/emacs/home/.emacs.d/elpa/loc-changes-20160801.1008/loc-changes-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "loc-changes" "20160801.1008" "keep track of positions even after buffer changes" 'nil :commit "4d1dcdf7631c23b1259ad4f72bf9686cf95fb46c" :url "http://github.com/rocky/emacs-loc-changes")
