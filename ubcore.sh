209b213e64dd2d90fd622aefd642aa8eee3c4fbe
mirage335 <spamfreemirage335@gmail.com>
2021-11-04 22:58:30 -0400

    Correction.

--- ubcore.sh
+++ ubcore.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='4055331462'
+export ub_setScriptChecksum_contents='4015303468'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -21826,6 +21826,10 @@ fi
 
 
 
-[[ "$1" == '_'* ]] && "$@"
+if [[ "$1" == '_'* ]]
+then
+	"$@"
+	exit "$?"
+fi
 _main "$@"
 
