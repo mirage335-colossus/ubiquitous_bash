8e268161da0e1e35053501eba1459a616ee410b6
mirage335 <spamfreemirage335@gmail.com>
2022-03-10 15:05:37 -0500

    Correction.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='1897194748'
+export ub_setScriptChecksum_contents='708564420'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -13855,6 +13855,14 @@ _persistentQemu() {
 }
 
 _testVBox() {
+	if [[ -e /etc/issue ]] && cat /etc/issue | grep 'Debian' > /dev/null 2>&1
+	then
+		if ! dpkg -l | grep linux-headers-$(uname -r) > /dev/null 2>&1
+		then
+			sudo -n apt-get install -y linux-headers-$(uname -r)
+		fi
+	fi
+	
 	_getDep VirtualBox
 	_getDep VBoxSDL
 	_getDep VBoxManage
@@ -13863,6 +13871,8 @@ _testVBox() {
 	#sudo -n checkDep dkms
 	
 	! _noFireJail virtualbox && _stop 1
+	
+	return 0
 }
 
 
--- virtualization/vbox/vboxtest.sh
+++ virtualization/vbox/vboxtest.sh
@@ -1,4 +1,12 @@
 _testVBox() {
+	if [[ -e /etc/issue ]] && cat /etc/issue | grep 'Debian' > /dev/null 2>&1
+	then
+		if ! dpkg -l | grep linux-headers-$(uname -r) > /dev/null 2>&1
+		then
+			sudo -n apt-get install -y linux-headers-$(uname -r)
+		fi
+	fi
+	
 	_getDep VirtualBox
 	_getDep VBoxSDL
 	_getDep VBoxManage
@@ -7,4 +15,6 @@ _testVBox() {
 	#sudo -n checkDep dkms
 	
 	! _noFireJail virtualbox && _stop 1
+	
+	return 0
 }
