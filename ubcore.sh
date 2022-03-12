ee3deaf725110177ab5aff78c805f693326ab498
mirage335 <spamfreemirage335@gmail.com>
2022-03-11 19:11:32 -0500

    Correction.

--- ubcore.sh
+++ ubcore.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='122970546'
+export ub_setScriptChecksum_contents='1587687237'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -10680,7 +10680,7 @@ _test_devgnuoctave-extra() {
 	local hostArch
 	hostArch=$(uname -m)
 	
-	if [[ "$hostArch" != "x86_64" ]] && [[ -e /etc/issue ]] && ! cat /etc/issue | grep 'Ubuntu' > /dev/null 2>&1
+	if [[ "$hostArch" == "x86_64" ]] && [[ -e /etc/issue ]] && ! cat /etc/issue | grep 'Ubuntu' > /dev/null 2>&1
 	then
 		_test_devgnuoctave_wantGetDep-octavePackage-internal symbolic
 		_test_devgnuoctave_wantGetDep-octavePackage-debian-x64 quaternion
