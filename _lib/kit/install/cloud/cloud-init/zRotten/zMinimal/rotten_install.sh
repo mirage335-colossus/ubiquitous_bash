57be6dbbedbccc98152ca800f75350098a1c09a1
mirage335 <spamfreemirage335@gmail.com>
2023-11-08 15:24:29 -0500

    Draft.

--- _lib/kit/install/cloud/cloud-init/zRotten/zMinimal/rotten_install.sh
+++ _lib/kit/install/cloud/cloud-init/zRotten/zMinimal/rotten_install.sh
@@ -472,6 +472,7 @@ _custom_kde() {
 	mkdir -p "$HOME"/.local/share/applications
 	mkdir -p "$HOME"/.local/share/icons
 	_messagePlain_probe_cmd cp -a "$currentBackupDir"/.local/share/applications/. "$HOME"/.local/share/applications/
+	_messagePlain_probe_cmd sudo -n cp -a "$currentBackupDir"/.local/share/applications/. "$HOME"/.local/share/applications/
 	_messagePlain_probe_cmd cp -a "$currentBackupDir"/.local/share/icons/. "$HOME"/.local/share/icons/
 
 	mkdir -p "$HOME"/.local/state
