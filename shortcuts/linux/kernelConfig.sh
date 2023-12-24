728ec168879a88340ae9db93a0a6804f7829d37a
mirage335 <spamfreemirage335@gmail.com>
2023-12-23 19:26:00 -0500

    Draft.

--- shortcuts/linux/kernelConfig.sh
+++ shortcuts/linux/kernelConfig.sh
@@ -239,7 +239,7 @@ _kernelConfig_require-tradeoff-harden() {
 	_kernelConfig__bad-n__ CONFIG_UBSAN_BOOL
 	_kernelConfig__bad-n__ CONFIG_UBSAN_ENUM
 	_kernelConfig__bad-n__ CONFIG_UBSAN_ALIGNMENT
-	_kernelConfig__bad-n__ # This is only available on Clang builds, and is likely already enabled if CONFIG_UBSAN_BOUNDS=y is set:
+	# This is only available on Clang builds, and is likely already enabled if CONFIG_UBSAN_BOUNDS=y is set:
 	_kernelConfig__bad-y__ CONFIG_UBSAN_LOCAL_BOUNDS
 	
 	
@@ -255,7 +255,7 @@ _kernelConfig_require-tradeoff-harden() {
 	
 	
 	_kernelConfig__bad-y__ CONFIG_STACKPROTECTOR
-	_kernelConfig__bad-y__ CONFIG_CC_STACKPROTECTOR_STRONG
+	_kernelConfig__bad-y__ CONFIG_STACKPROTECTOR_STRONG
 	
 	
 	_kernelConfig__bad-n__ CONFIG_DEVKMEM
--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='345443898'
+export ub_setScriptChecksum_contents='3597914892'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -31597,7 +31597,7 @@ _kernelConfig_require-tradeoff-harden() {
 	_kernelConfig__bad-n__ CONFIG_UBSAN_BOOL
 	_kernelConfig__bad-n__ CONFIG_UBSAN_ENUM
 	_kernelConfig__bad-n__ CONFIG_UBSAN_ALIGNMENT
-	_kernelConfig__bad-n__ # This is only available on Clang builds, and is likely already enabled if CONFIG_UBSAN_BOUNDS=y is set:
+	# This is only available on Clang builds, and is likely already enabled if CONFIG_UBSAN_BOUNDS=y is set:
 	_kernelConfig__bad-y__ CONFIG_UBSAN_LOCAL_BOUNDS
 	
 	
@@ -31613,7 +31613,7 @@ _kernelConfig_require-tradeoff-harden() {
 	
 	
 	_kernelConfig__bad-y__ CONFIG_STACKPROTECTOR
-	_kernelConfig__bad-y__ CONFIG_CC_STACKPROTECTOR_STRONG
+	_kernelConfig__bad-y__ CONFIG_STACKPROTECTOR_STRONG
 	
 	
 	_kernelConfig__bad-n__ CONFIG_DEVKMEM
