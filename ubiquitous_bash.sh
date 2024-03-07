277da2e80f62bfb668fdb2e76fd7b6e399abe64c
mirage335 <spamfreemirage335@gmail.com>
2024-03-06 21:16:57 -0500

    Correction.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='3454878510'
+export ub_setScriptChecksum_contents='1729703390'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -18648,8 +18648,8 @@ _createVMfstab() {
 	
 	# WARNING: May be untested.
 	echo '' | sudo -n tee -a "$globalVirtFS"/etc/fstab
-	echo '#tmpfs /var/spool/cups tmpfs defaults,uid=0,gid=7,umask=007,dmask=007,fmask=117 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
-	echo '#tmpfs /var/cache/cups tmpfs defaults,uid=0,gid=7,umask=007,dmask=000,fmask=007 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
+	echo '#none /var/spool/cups ramfs defaults,uid=0,gid=7,umask=007,dmask=007,fmask=117,size=800M 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
+	echo '#none /var/cache/cups ramfs defaults,uid=0,gid=7,umask=007,dmask=000,fmask=007,size=800M 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
 	echo '' | sudo -n tee -a "$globalVirtFS"/etc/fstab
 	
 	return 0
--- virtualization/bios/createvm.sh
+++ virtualization/bios/createvm.sh
@@ -479,8 +479,8 @@ _createVMfstab() {
 	
 	# WARNING: May be untested.
 	echo '' | sudo -n tee -a "$globalVirtFS"/etc/fstab
-	echo '#tmpfs /var/spool/cups tmpfs defaults,uid=0,gid=7,umask=007,dmask=007,fmask=117 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
-	echo '#tmpfs /var/cache/cups tmpfs defaults,uid=0,gid=7,umask=007,dmask=000,fmask=007 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
+	echo '#none /var/spool/cups ramfs defaults,uid=0,gid=7,umask=007,dmask=007,fmask=117,size=800M 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
+	echo '#none /var/cache/cups ramfs defaults,uid=0,gid=7,umask=007,dmask=000,fmask=007,size=800M 0 0' | sudo -n tee -a "$globalVirtFS"/etc/fstab
 	echo '' | sudo -n tee -a "$globalVirtFS"/etc/fstab
 	
 	return 0
