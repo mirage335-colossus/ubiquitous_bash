f63f05b3b495a34ce0ebb24ba93f1f165cd25f2e
mirage335 <spamfreemirage335@gmail.com>
2025-03-09 14:44:35 -0400

    Draft - set Cygwin packages explicitly. Upstream.

--- _local/ubcp/ubcp-cygwin-portable-installer.cmd
+++ _local/ubcp/ubcp-cygwin-portable-installer.cmd
@@ -47,7 +47,8 @@ set CYGWIN_USERNAME=root
 
 :: ATTENTION: NOTICE: Additional packages may be installed by commands elsewhere!
 :: select the packages to be installed automatically via apt-cyg
-set CYGWIN_PACKAGES=bash-completion,bc,bzip,coreutils,curl,dos2unix,expect,git,git-svn,gnupg,inetutils,util-linux,jq,lz4,mc,nc,openssh,openssl,perl,psmisc,python37,pv,rsync,ssh-pageant,screen,subversion,unzip,vim,wget,zip,zstd,tigervnc-server,flex,bison,libncurses-devel,par2,python3-pip,gnupg2
+REM set CYGWIN_PACKAGES=bash-completion,bc,bzip,coreutils,curl,dos2unix,expect,git,git-svn,gnupg,inetutils,util-linux,jq,lz4,mc,nc,openssh,openssl,perl,psmisc,python37,pv,rsync,ssh-pageant,screen,subversion,unzip,vim,wget,zip,zstd,tigervnc-server,flex,bison,libncurses-devel,par2,python3-pip,gnupg2
+set CYGWIN_PACKAGES=bash-completion,bc,bzip,coreutils,curl,dos2unix,expect,git,git-svn,ca-certificates,gnupg,inetutils,util-linux,jq,lz4,mc,nc,openssh,openssl,perl,psmisc,python37,pv,rsync,ssh-pageant,screen,subversion,unzip,vim,libiconv,wget,zip,zstd,procps-ng,awk,socat,aria2,jq,gnupg2,php,php-PEAR,php-devel,gnuplot-base,gnuplot-doc,gnuplot-qt5,gnuplot-wx,gnuplot-X11,libqalculate-common,libqalculate-devel,libqalculate5,cantor-backend-qalculate,octave,octave-devel,octave-parallel,octave-linear-algebra,octave-general,octave-geometry,octave-strings,octave-financial,octave-communications,octave-control,mkisofs,genisoimage,dbus,dbus-x11,tigervnc-server,flex,bison,libncurses-devel,p7zip,par2,python3-pip,python37-jinja2,python37-six,python37-yaml,bind-utils
 
 :: if set to 'yes' the local package cache created by cygwin setup will be deleted after installation/update
 set DELETE_CYGWIN_PACKAGE_CACHE=no
