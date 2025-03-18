b0ee51711f60b02dbc10d0adcf7996780e84726c
mirage335 <spamfreemirage335@gmail.com>
2025-03-18 01:55:34 -0400

    Draft.

--- compile.sh
+++ compile.sh
@@ -36,7 +36,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='2591634041'
-export ub_setScriptChecksum_contents='3203827443'
+export ub_setScriptChecksum_contents='2564686148'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -8693,18 +8693,18 @@ _backend_override() {
 	! type -f _backend > /dev/null 2>&1 && _backend() { "$@" ; unset -f _backend ; }
 	_backend "$@"
 }
-# ...
-# EXAMPLE
-# ! _openChRoot && _messageFAIL
-# ...
+## ...
+## EXAMPLE
+#! _openChRoot && _messageFAIL
+## ...
 #_backend() {
 	#_ubdistChRoot "$@"
 	##"$@"
 #}
-# ...
+## ...
 #_backend_override echo test
-# ...
-# ! _closeChRoot && _messageFAIL
+## ...
+#! _closeChRoot && _messageFAIL
 #unset -f _backend
 
 
