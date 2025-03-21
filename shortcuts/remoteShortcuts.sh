c944dd6974ae43d057f020e37cd8764f3c08997b
mirage335 <spamfreemirage335@gmail.com>
2025-03-21 15:34:55 -0400

    Untracked file.

---
+++ shortcuts/remoteShortcuts.sh
@@ -0,0 +1,37 @@
+
+
+# _upgrade-import-assets corpName
+_upgrade-import-assets() {
+    local corpName="$1"
+
+
+	_start
+
+	cd "$safeTmp"
+
+	if ! ( type _gitBest > /dev/null 2>&1 && _gitBest clone --depth 1 'git@github.com:soaringDistributions/zImport_corp_'"$corpName"'.git' )
+	then
+		if ls -1 "$HOME"/.ssh/id_* > /dev/null
+		then
+			if ! git clone --depth 1 'git@github.com:soaringDistributions/zImport_corp_'"$corpName"'.git'
+			then
+				_messagePlain_bad 'bad: upgrade-import-assets-'"$corpName"': git: FAIL: fail'
+				_messageFAIL
+				_stop 1
+				exit 1
+		else
+			_messagePlain_bad 'bad: upgrade-import-assets-'"$corpName"': git: FAIL: no remote permissions'
+			_messageFAIL
+			_stop 1
+			exit 1
+		fi
+	fi
+
+	mkdir -p "$scriptLib"/zImport_corp_"$corpName"
+	mv -f "$safeTmp"/zImport_corp_"$corpName"/*.sh "$scriptLib"/zImport_corp_"$corpName"/
+	mv -f "$safeTmp"/zImport_corp_"$corpName"/*.yml "$scriptLib"/zImport_corp_"$corpName"/
+	mv -f "$safeTmp"/zImport_corp_"$corpName"/*.txt "$scriptLib"/zImport_corp_"$corpName"/
+	mv -f "$safeTmp"/zImport_corp_"$corpName"/*.md "$scriptLib"/zImport_corp_"$corpName"/
+
+	_stop
+}
