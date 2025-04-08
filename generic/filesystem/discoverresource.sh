c822f08da81b08e43711afecfacf90d6213b000b
quidscio <rmhines@polaris360.com>
2025-04-07 22:18:15 -0400

    comment1 update

--- generic/filesystem/discoverresource.sh
+++ generic/filesystem/discoverresource.sh
@@ -1,3 +1,6 @@
+# Discovers resource files by searching in current and up to 3 parent directories
+# Inputs include a filename to search for and the script's absolute folder 
+# Echos the full path to the file if found with return code=0, return code=1 otherwise 
 _discoverResource() {
 	
 	[[ "$scriptAbsoluteFolder" == "" ]] && return 1
