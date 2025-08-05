e54917d52d526a1b1e4951d3cf09964631dd6454
mirage335 <spamfreemirage335@gmail.com>
2025-08-05 07:23:49 +0100

    Compile.

--- ubcore.sh
+++ ubcore.sh
@@ -39,11 +39,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='3620520443'
-<<<<<<< HEAD
-export ub_setScriptChecksum_contents='1786462890'
-=======
-export ub_setScriptChecksum_contents='2533282470'
->>>>>>> 59dc7258af7e4a0bf4ba0cd4ac889b6dca52d303
+export ub_setScriptChecksum_contents='1253494442'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
