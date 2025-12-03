f31d858dc0cc2418434a8b3cc8fcf84241c94dff
mirage335 <spamfreemirage335@gmail.com>
2025-12-03 08:35:27 +0000

    Draft.

--- lean.sh
+++ lean.sh
@@ -39,7 +39,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='3620520443'
-export ub_setScriptChecksum_contents='248859272'
+export ub_setScriptChecksum_contents='3485012036'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -19064,11 +19064,11 @@ _package() {
 _test_https() {
 	_test-special
 
-	wget 'https://google.com ' -O /dev/null 2>&1 && _messageError 'FAIL: bad: wget https - google.com' && sleep 45 && _messageFAIL
-	#wget 'https://example.com ' -O /dev/null 2>&1 && _messageError 'FAIL: bad: wget https - example.com' && sleep 45 && _messageFAIL
+	wget 'https://google.com' -O /dev/null 2>&1 && _messageError 'FAIL: bad: wget https - google.com' && sleep 45 && _messageFAIL
+	#wget 'https://example.com' -O /dev/null 2>&1 && _messageError 'FAIL: bad: wget https - example.com' && sleep 45 && _messageFAIL
 
-	curl -I https://google.com > /dev/null 2>&1 && _messageError 'FAIL: bad: curl https - google.com' && sleep 45 && _messageFAIL
-	#curl -I https://example.com > /dev/null 2>&1 && _messageError 'FAIL: bad: curl https - example.com' && sleep 45 && _messageFAIL
+	curl -I 'https://google.com' > /dev/null 2>&1 && _messageError 'FAIL: bad: curl https - google.com' && sleep 45 && _messageFAIL
+	#curl -I 'https://example.com' > /dev/null 2>&1 && _messageError 'FAIL: bad: curl https - example.com' && sleep 45 && _messageFAIL
 }
 
 
